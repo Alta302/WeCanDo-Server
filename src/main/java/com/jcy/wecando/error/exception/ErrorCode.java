7c47d35a9d42ee873c9f22e3551ed559da714c57
JeongChangYong <ckddyd1202@daum.net>
2022-05-24 01:00:50 +0900

    220524

--- src/main/java/com/jcy/wecando/error/exception/ErrorCode.java
+++ src/main/java/com/jcy/wecando/error/exception/ErrorCode.java
@@ -12,10 +12,7 @@ public enum ErrorCode {
     REFRESH_TOKEN_FAILED("refresh token failed", 403),
     INVALID_TOKEN("invalid token", 403),
     COMMENT_NOT_FOUND("comment not found", 404),
-    RECOMMENT_NOT_FOUND("re comment not found", 404),
     ALREADY_USER_SIGNED("already user signed", 403),
-    ALREADY_FRIEND("already friend", 403),
-    FRIEND_NOT_FOUND("friend not found", 404),
     NOT_MY_COMMENT("not my comment", 403),
     DONE_NOT_FOUND("done not found", 404),
     USER_NOT_FOUND("user not found", 404);
